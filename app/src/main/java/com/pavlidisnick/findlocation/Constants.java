21e631c799f19cfd5f3c4f776d75f30402df1478
pavlidisnick <pavlidis.nik95@gmail.com>
2020-09-26 12:19:40 +0300

    Initial commit

---
+++ app/src/main/java/com/pavlidisnick/findlocation/Constants.java
@@ -0,0 +1,11 @@
+package com.pavlidisnick.findlocation;
+
+class Constants {
+
+    private static final String PACKAGE_NAME = "com.pavlidisnick.findlocation";
+    static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
+    static final String RECEIVER = PACKAGE_NAME + ".REVEIVER";
+    static final String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
+    static final int SUCCESS_RESULT = 1;
+    static final int FAILURE_RESULT = 0;
+}
